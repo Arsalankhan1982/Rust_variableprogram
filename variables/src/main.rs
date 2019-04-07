cb4ec16311892253c42073889e28a0b09e9b85ad
Arsalankhan1982 <arsalankhan1982@gmail.com>
2019-04-07 18:32:32 +0500

    Variable Program in Rust

---
+++ variables/src/main.rs
@@ -0,0 +1,12 @@
+/*fn main() {
+    let mut x = 5;
+    println!("The Value of x is : {}",x);
+    x = 6;
+    println!("The Value of x is : {}",x);
+}*/
+fn main() {
+for number in (1..4).rev() {
+println!("{}!", number);
+}
+println!("LIFTOFF!!!");
+}
